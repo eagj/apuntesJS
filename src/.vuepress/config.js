506778f24ad1a286e5912ab32457f1325f21b376
quique <quique@eagj.net>
2021-12-27 14:22:38 +0100

    primer commit

---
+++ src/.vuepress/config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  title: 'Apunte sobre JS',
+  description: 'Mis apuntes de JS según voy aprendiendo',
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+  themeConfig: {
+    editLinks: false,
+    editLinkText: '',
+    lastUpdated: false,
+    dest: 'docs',
+    base: '/apuntesJS/',
+    nav: [
+      {
+        text: 'Guía',
+        link: '/',
+      },     
+    ],
+    sidebar: [
+      '/',
+      '/01-Variables/'
+    ]
+    
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+}
